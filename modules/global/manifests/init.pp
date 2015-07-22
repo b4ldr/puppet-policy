a63927dfe5bb7d463edce24e031eab3db20cabb9
b4ldr <github@johnbond.org>
2015-07-22 11:11:52 +0200

    add packages and puppet run script

--- modules/global/manifests/init.pp
+++ modules/global/manifests/init.pp
@@ -1,8 +1,15 @@
 # Global defaults
 #
 class global {
+  package {['vim','puppet']:
+    ensure => latest,
+  }
   file { '/srv/puppet/puppet-policy/hiera.yaml':
     ensure  => present,
     content => template('global/srv/puppet/puppet-policy/hiera.yaml.erb'),
+  }
+  file { '/usr/local/bin/run_puppet.sh':
+    ensure  => present,
+    content => template('global/usr/local/bin/run_puppet.sh.erb'),
   }
 }
