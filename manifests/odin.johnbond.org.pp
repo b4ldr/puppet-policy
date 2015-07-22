de9f91ffc24168aa92002294acade68dbcf1c786
b4ldr <github@johnbond.org>
2015-07-22 12:27:35 +0200

    add openssl cert

--- manifests/odin.johnbond.org.pp
+++ manifests/odin.johnbond.org.pp
@@ -1,8 +1,15 @@
 node default {
   class { '::global': }
   class { '::prosody': }
-  prosody::virtualhost {'johnbond.org':
-    ensure => present,
+  openssl::certificate::x509 {$::fqdn:
+    country      => 'UK',
+    organization => $::fqdn,
+    commonname   => $::fqdn,
   }
+  prosody::virtualhost {'johnbond.org':
+    ensure  => present,
+    require => Openssl::Certificate::X509[$::fqdn],
+  }
+
 
 }
