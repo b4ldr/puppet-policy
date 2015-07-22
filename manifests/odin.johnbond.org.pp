e44199a61ac5baebcef0c74c188d3f7c82dda361
b4ldr <github@johnbond.org>
2015-07-22 12:44:03 +0200

    move virtual host into yaml

--- manifests/odin.johnbond.org.pp
+++ manifests/odin.johnbond.org.pp
@@ -7,10 +7,4 @@ node default {
     organization => $::fqdn,
     commonname   => $::fqdn,
   }
-  prosody::virtualhost {'johnbond.org':
-    ensure   => present,
-    ssl_key  => '/etc/ssl/certs/odin.johnbond.org.key',
-    ssl_cert => '/etc/ssl/certs/odin.johnbond.org.crt',
-    require  => Openssl::Certificate::X509[$::fqdn],
-  }
 }
