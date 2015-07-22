e1b6ba095fbbe2bceb988f7ee443a71af96b4d13
b4ldr <github@johnbond.org>
2015-07-22 12:35:17 +0200

    change certificat permission

--- manifests/odin.johnbond.org.pp
+++ manifests/odin.johnbond.org.pp
@@ -2,13 +2,13 @@ node default {
   class { '::global': }
   class { '::prosody': }
   openssl::certificate::x509 {$::fqdn:
+    owner        => 'prosody',
     country      => 'UK',
     organization => $::fqdn,
     commonname   => $::fqdn,
   }
   prosody::virtualhost {'johnbond.org':
     ensure   => present,
-    owner    => 'prosody',
     ssl_key  => '/etc/ssl/certs/odin.johnbond.org.key',
     ssl_cert => '/etc/ssl/certs/odin.johnbond.org.crt',
     require  => Openssl::Certificate::X509[$::fqdn],
