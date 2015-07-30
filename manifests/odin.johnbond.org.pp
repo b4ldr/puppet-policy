49ba29e3e6cd4d95dfc8fb6ba2db567e8ceae885
b4ldr <github@johnbond.org>
2015-07-30 12:15:39 +0100

    update worker processes

--- manifests/odin.johnbond.org.pp
+++ manifests/odin.johnbond.org.pp
@@ -2,6 +2,11 @@ node default {
   class { '::global': }
   class { '::gitlab': }
   class { '::prosody': }
+  openssl::certificate::x509 {'mail.b4ldr.com':
+    country      => 'UK',
+    organization => 'b4ldr',
+    commonname   => 'mail.b4ldr.com',
+  }
   openssl::certificate::x509 {$::fqdn:
     owner        => 'prosody',
     country      => 'UK',
